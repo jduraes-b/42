a86aa407be2eb4ba0b0ff4562b9210faf53d91f1
Joao Duraes Barros <jduraes-@c1r5s7.42porto.com>
2023-07-10 00:16:53 +0100

    updating pipex...

--- pipex/pipex_utils.c
+++ pipex/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 20:22:30 by jduraes-          #+#    #+#             */
-/*   Updated: 2023/07/06 20:55:41 by jduraes-         ###   ########.fr       */
+/*   Updated: 2023/07/09 22:34:42 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ char	**findpaths(char **envp)
 	i = 0;
 	while(!ft_strnstr(envp[i], "PATH=", 5))
 		i++;
-	actualpath = ft_substr(envp[i], 5, ft_strlen(envp[i]);
-	paths = ft_split(actualpath + 5, ":");
+	actualpath = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+	paths = ft_split(actualpath + 5, ':');
 	return (paths);
 }
 
-char	*getcmdpath(char **paths, char *cmd)
+char	*getcmdpath(char **paths, char *cmd, char **envp)
 {
-	char	*path
+	char	*path;
 	int	i;
 
 	i = 0;
@@ -37,15 +37,52 @@ char	*getcmdpath(char **paths, char *cmd)
 		path = ft_strjoin(envp[i], "/");
 		path = ft_strjoin(path, cmd);
 		if (access(path, F_OK) == 0)
+		{
+			doublefree(paths);
 			return (path);
+		}
 		free(path);
 		i++;
 	}
+	doublefree(paths);
 	ft_printf("Error\n");
 	return (NULL);
 }
-/*
-void	execute(char **paths, char *cmd)
-{
 
-}*/
+void	doublefree(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+void	execute(char *cmd, char **envp)
+{
+	char	*cmdpath;
+	char	**cmdprep;
+	char	**paths;
+
+	paths = findpaths(envp);
+	cmdpath = getcmdpath(paths, cmd, envp);
+	cmdprep = ft_split(cmd, 45);
+	if (!cmdpath)
+	{
+		free(cmdpath);
+		doublefree(cmdprep);
+		perror("path");
+		exit(EXIT_FAILURE);
+	}
+	if (execve(cmdpath, cmdprep, envp) == -1)
+	{
+		doublefree(cmdprep);
+		free(cmdpath);
+		perror("execution");
+		exit(EXIT_FAILURE);
+	}
+}
