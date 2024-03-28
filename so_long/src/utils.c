afcf0204e5bbb5a8e25aab3249866d09cc90bacb
Joao Duraes Barros <jduraes-@c2r4s8.42porto.com>
2024-03-28 18:41:04 +0000

    actually done now

--- so_long/src/utils.c
+++ so_long/src/utils.c
@@ -12,12 +12,12 @@
 
 #include "so_long.h"
 
-int	hack(int i, int j, t_data *gamestate)
+int	manobra(int i, int j, t_data *gamestate)
 {
 	char	c;
 
 	c = gamestate->map[i][j];
-	if (c != '1' && c != '0' && c != 'E' && c != 'C')
+	if (c != '1' && c != '0' && c != 'E' && c != 'C' && c != 'P')
 		gamestate->xlen = -1;
 	return (0);
 }
