51f40e44289be65a22b2c04bc4ca786ba2b8a4f0
Joao Duraes Barros <jduraes-@c2r2s8.42porto.com>
2024-06-19 22:05:59 +0100

    philowork

--- philo/dinner.c
+++ philo/dinner.c
@@ -6,11 +6,62 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 18:54:43 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/06/18 19:18:07 by jduraes-         ###   ########.fr       */
+/*   Updated: 2024/06/19 22:05:43 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	oddfork(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->forkmut[philo->nr % philo->table->pc]);
+    if (!action(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr % philo->table->pc]);
+		return (1);
+	}
+	pthread_mutex_lock(&philo->table->forkmut[philo->nr - 1]);
+	if (!action(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr - 1]);
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr % philo->table->pc]);
+		return (1);
+	}
+	return (0);
+}
+
+int	evenfork(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->forkmut[philo->nr - 1]);
+	if (!action(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr - 1]);
+		return (1);
+	}
+	pthread_mutex_lock(&philo->table->forkmut[philo->nr % philo->table->pc]);
+    if (!action(philo, "has taken a fork"))
+	{
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr % philo->table->pc]);
+		pthread_mutex_unlock(&philo->table->forkmut[philo->nr - 1]);
+		return (1);
+	}
+
+	return (0);
+}
+
+int	grabfork(t_philo *philo)
+{
+	if (philo->table->pc == 1)
+		return (single(philo));
+	else if (philo->nr % 2 == 0)
+	{
+		if (evenfork(philo))
+			return (0);
+		if (oddfork(philo))
+			return (0);
+	}
+	return (1);
+}
 
 void *dinner(void *phil)
 {
@@ -19,7 +70,8 @@ void *dinner(void *phil)
     philo = (t_philo *)phil;
     while (1)
 	{
-		
+		if (!grabfork(philo))
+		    return (NULL);
     }
     return ;
 }
@@ -31,7 +83,15 @@ int	start(t_table *table)
 	while (++i < table->pc)
 	{
 		if (pthread_create(&table->philos[i].thread, NULL, dinner, &table->philos[i]))
-		    error("thread creation error");
+		    error("thread creation error", table);
 
 	}
+	monitoring(table);
+	i = -1;
+	while (++i < table->pc)
+	{
+		if (pthread_join(table->philos[i].thread, NULL))
+            error("thread join error", table);
+	}
+
 }
