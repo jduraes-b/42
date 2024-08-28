358cb1ce5c6884d7a6114e96815d4869de7d4889
Joao Duraes Barros <jduraes-@c2r4s2.42porto.com>
2024-08-28 20:48:30 +0100

    progress

---
+++ c++/CPP 01/ex03/humanA.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   humanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 20:05:18 by jduraes-          #+#    #+#             */
+/*   Updated: 2024/08/28 20:27:25 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include <iostream>
+#include <string>
+#include "Weapon.hpp"
+
+class humanA
+{
+	private:
+		std::string name;
+		Weapon &weapon;
+	public:
+		humanA(std::string name, Weapon &weapon);
+		~humanA();
+		void attack(void);
+};
+
+#endif
