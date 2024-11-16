3dd87470b55ba280d8cfed5b598a5ac4bddccba1
Joao Duraes Barros <jduraes-@c2r5s8.42porto.com>
2024-11-16 19:49:20 +0000

    updating cpp

--- c++/CPP 01/ex01/main.cpp
+++ c++/CPP 01/ex01/main.cpp
@@ -5,34 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/28 19:28:10 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/11/16 19:20:18 by jduraes-         ###   ########.fr       */
+/*   Created: 2024/08/27 19:59:59 by jduraes-          #+#    #+#             */
+/*   Updated: 2024/11/14 19:28:43 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Weapon.hpp"
-#include "humanA.hpp"
-#include "humanB.hpp"
+#include "Zombie.hpp"
+#include <sstream>
 
-int main()
+int	main(int argc, char **argv)
 {
+	Zombie	*Horde;
+	int		n;
+	
+	if (argc == 3 && argv[1])
 	{
-		/*humanA carl("Carl");
-		humanA charles;*/
-		Weapon club = Weapon("crude spiked club");
-		humanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
+		std::stringstream ss(argv[1]);
+		ss >> n;
+		if (ss.fail() || n < 0 || n > 1000)
+		{
+			std::cout << "Wrong number of zombies. Available 1 to 1000 zombie range" << std::endl;
+			return (1);
+		}
+		Horde = zombieHorde(n, argv[2]);
+		for (int i = 0; i < n; i++)
+			Horde[i].announce();
+		delete[] Horde;
+		return (0);
 	}
-	{
-		Weapon club = Weapon("crude spiked club");
-		humanB jim("Jim");
-		jim.attack();
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
-return 0;
+	std::cout << "Usage: ./moarBrainz <number_of_zombies> <zombie_name>" << std::endl;
+	return (1);
 }
--- c++/CPP 01/ex06/Harl.hpp
+++ c++/CPP 01/ex06/Harl.hpp
@@ -1,33 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/27 19:59:59 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/10/18 19:50:09 by jduraes-         ###   ########.fr       */
+/*   Created: 2024/09/03 20:06:44 by jduraes-          #+#    #+#             */
+/*   Updated: 2024/11/16 18:58:29 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-#include <sstream>
+#include <iostream>
+#include <string>
 
-int	main(int argc, char **argv)
+class Harl
 {
-	Zombie	*Horde;
-	int		n;
-	
-	if (argc == 3)
-	{
-		std::stringstream ss(argv[1]);
-		ss >> n;
-		Horde = zombieHorde(n, argv[2]);
-		for (int i = 0; i < n; i++)
-			Horde[i].announce();
-		delete[] Horde;
-		return (0);
-	}
-	std::cout << "Usage: ./moarBrainz <number_of_zombies> <zombie_name>" << std::endl;
-	return (1);
-}
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+
+};
