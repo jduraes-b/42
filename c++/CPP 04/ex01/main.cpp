4b5052c56ee8ae560c849ec79e574b5669cfbca3
jduraes <jdbarros99@gmail.com>
2024-12-05 21:03:59 +0000

    cpp 0-4 done

--- c++/CPP 04/ex01/main.cpp
+++ c++/CPP 04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:08:27 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/09/24 19:27:52 by jduraes-         ###   ########.fr       */
+/*   Updated: 2024/12/01 19:41:39 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int main()
 
 	j->makeSound(); // This should output: Woof!
 	i->makeSound(); // This should output: Miau!
+	std::cout << std::endl;
 
 	const Animal *meta[10];
 
@@ -40,13 +41,14 @@ int main()
 		delete meta[i];
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 	Dog *dog = new Dog();
 	dog->setIdea(0, "I am a dog");
 	Dog *dogclone = new Dog(*dog);
 	dog->setIdea(0, "Am I a dog?");
-	std::cout << dogclone->getIdea(0) << std::endl;
-	std::cout << dog->getIdea(0) << std::endl;
-
+	std::cout << "dogclone: " << dogclone->getIdea(0) << std::endl;
+	std::cout << "dog: " << dog->getIdea(0) << std::endl;
+	std::cout << std::endl;
 
 	delete j;
 	delete i;
---
+++ c++/CPP 04/ex03/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 19:53:52 by jduraes-          #+#    #+#             */
+/*   Updated: 2024/12/05 20:58:06 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+#include "MateriaSource.hpp"
+#include "Character.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+/*class Ice;
+class Cure;*/
+
+int main()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	
+	ICharacter* me = new Character("me");
+	
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	
+	ICharacter* bob = new Character("bob");
+	
+	me->use(0, *bob);
+	me->use(1, *bob);
+	
+	delete bob;
+	delete me;
+	delete src;
+
+	std::cout << "other tests: " <<std::endl;
+
+	ICharacter*	Bill = new Character("Bill");
+	IMateriaSource *src2 = new MateriaSource();
+
+	src2->learnMateria(new Ice());
+	src2->learnMateria(new Cure());
+	tmp = src2->createMateria("ice");
+	Bill->equip(tmp);
+	tmp = src2->createMateria("cure");
+	Bill->equip(tmp);
+	tmp = src2->createMateria("ice");
+	Bill->equip(tmp);
+	tmp = src2->createMateria("cure");
+	Bill->equip(tmp);
+	tmp = src2->createMateria("ice");
+	Bill->equip(tmp);
+	Bill->unequip(2);
+	Bill->use(1, *Bill);
+
+	delete src2;
+	delete Bill;
+	
+	return 0;
+}
