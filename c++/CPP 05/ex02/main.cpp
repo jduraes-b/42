d8ffb520e651e18da1d5e47d177f25cf29ef619b
jduraes <jdbarros99@gmail.com>
2025-01-07 19:44:16 +0000

    cpp work

--- c++/CPP 05/ex02/main.cpp
+++ c++/CPP 05/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 19:25:16 by jduraes-          #+#    #+#             */
-/*   Updated: 2025/01/05 19:40:44 by jduraes-         ###   ########.fr       */
+/*   Updated: 2025/01/07 19:02:39 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include <string.h>
 
 int	main()
@@ -19,12 +20,18 @@ int	main()
 	Bureaucrat	b1("Carl", 150);
 	Bureaucrat	b2("Karl", 1);
 
-	ShrubberyCreationForm	f1("1A");
+	ShrubberyCreationForm	f1("../");
+	RobotomyRequestForm	f2("Qarl");
 
 	b1.signForm(f1);
 	b2.signForm(f1);
 	b1.executeForm(f1);
 	b2.executeForm(f1);
+
+	b1.signForm(f2);
+	b2.signForm(f2);
+	b1.executeForm(f2);
+	b2.executeForm(f2);
 
 	return 0;
 }
