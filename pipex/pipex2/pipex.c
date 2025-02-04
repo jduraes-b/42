5166bb4d169ccf1b44816481c0a60045722ffac4
jduraes <jdbarros99@gmail.com>
2025-02-04 20:41:14 +0000

    commit

--- pipex/pipex2/pipex.c
+++ pipex/pipex2/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 18:23:32 by jduraes-          #+#    #+#             */
-/*   Updated: 2023/09/22 19:49:32 by jduraes-         ###   ########.fr       */
+/*   Updated: 2023/09/27 19:34:50 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ void	execute(char *cmd, char **envp)
 	char	*path;
 
 	cmdpparam = ft_split(cmd, 32);
-	path = findpath(cmdpparam[0], envp);
+	if (!cmd[0])
+		return;
+	if (cmd[0] == '/')
+		path = cmd;
+	else
+		path = findpath(cmdpparam[0], envp);
 	if (!path)
 	{
-		dup2(STDIN_FILENO, STDERR_FILENO);
-		ft_printf("%s: not found", cmdpparam[0]);
+		dup2(STDERR_FILENO, STDOUT_FILENO);
+		ft_printf("command not found: %s\n", cmdpparam[0]);
 		free(path);
 		doublefree(cmdpparam);
 		exit(127);
@@ -46,6 +51,11 @@ void	child(int *f, int *p, char **argv, char **envp)
 	close(p[0]);
 	dup2(f[0], STDIN_FILENO);
 	dup2(p[1], STDOUT_FILENO);
+	if (!argv[2][0])
+	{
+		dup2(STDERR_FILENO, STDOUT_FILENO);
+		ft_printf("Command '' not found\n");
+	}
 	execute(argv[2], envp);
 }
 
@@ -65,7 +75,12 @@ void	pipex(int *f, int *p, char **argv, char **envp)
 		child(f, p, argv, envp);
 	else
 	{
-		waitpid(pid, NULL, 0);
+		if (!argv[3][0])
+		{
+			dup2(STDERR_FILENO, STDOUT_FILENO);
+			ft_printf("Command '' not found\n");
+		}
+		waitpid(pid, NULL, WNOHANG);
 		close(p[1]);
 		dup2(p[0], STDIN_FILENO);
 		dup2(f[1], STDOUT_FILENO);
@@ -82,6 +97,10 @@ int	main(int argc, char **argv, char **envp)
 	{
 		f[0] = open(argv[1], O_RDONLY, 0644);
 		f[1] = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (f[1] == -1)
+		{
+			perror("outfile");
+		}
 		pipex(f, p, argv, envp);
 	}
 	else
---
+++ push_swap/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 18:43:45 by jduraes-          #+#    #+#             */
+/*   Updated: 2023/09/29 18:50:13 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+# include <stdarg.h>
+# include <stdbool.h>
+# include <limits.h>
+
+
