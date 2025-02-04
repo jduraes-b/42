d551596742172f26fd3080d8bf538d532e5b89f4
jduraes <jdbarros99@gmail.com>
2025-02-04 20:40:55 +0000

    some progress

--- pipex/pipex_utils.c
+++ pipex/pipex_utils.c
@@ -6,13 +6,13 @@
 /*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 20:22:30 by jduraes-          #+#    #+#             */
-/*   Updated: 2023/06/29 20:53:53 by jduraes-         ###   ########.fr       */
+/*   Updated: 2023/07/06 20:55:41 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*findepath(char	**envp)
+char	**findpaths(char **envp)
 {
 	int	i;
 	char	**paths;
@@ -21,22 +21,31 @@ char	*findepath(char	**envp)
 	i = 0;
 	while(!ft_strnstr(envp[i], "PATH=", 5))
 		i++;
-	paths = ft_split(envp[i] + 5, ":");
+	actualpath = ft_substr(envp[i], 5, ft_strlen(envp[i]);
+	paths = ft_split(actualpath + 5, ":");
+	return (paths);
+}
+
+char	*getcmdpath(char **paths, char *cmd)
+{
+	char	*path
+	int	i;
+
 	i = 0;
 	while (paths[i])
 	{
-		actualpath = ft_strjoin(actualpath, paths[i]);
-		actualpath = ft_strjoin(actualpath, "/");
+		path = ft_strjoin(envp[i], "/");
+		path = ft_strjoin(path, cmd);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
 		i++;
 	}
-	return (actualpath);
+	ft_printf("Error\n");
+	return (NULL);
 }
 /*
-void	execute(char *cmd, char **envp)
+void	execute(char **paths, char *cmd)
 {
-	char	**command;
-	char	*pathline;
-
-	pathline = ft_substr(envp, );
 
 }*/
