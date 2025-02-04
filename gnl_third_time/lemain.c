9a2a3e81b78432e9005a64cddd2f856bc41bf987
jduraes <jdbarros99@gmail.com>
2025-02-04 20:40:01 +0000

    yikes...

--- gnl_third_time/lemain.c
+++ gnl_third_time/lemain.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   lemain.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jduraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 20:34:48 by jduraes-          #+#    #+#             */
-/*   Updated: 2023/06/12 19:13:30 by jduraes-         ###   ########.fr       */
+/*   Updated: 2023/06/20 19:28:31 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	main(void)
 	char	*line;
 	int		fd;
 
-	fd = open("big_line_no_nl", O_RDONLY);
+	fd = open("only_nl.txt", O_RDONLY);
 	line = get_next_line(fd);
 	while (line)
 	{
-		printf("%s\n", line);
+		printf("\"%s\"\n", line);
 		free(line);
 		line = get_next_line(fd);
 	}
