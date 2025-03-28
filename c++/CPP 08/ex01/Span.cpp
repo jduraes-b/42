d048af6bf3ceccf919d878b0a177904db5b27f4b
jduraes <jdbarros99@gmail.com>
2025-03-28 19:56:08 +0000

    cpp work

--- c++/CPP 08/ex01/Span.cpp
+++ c++/CPP 08/ex01/Span.cpp
@@ -6,33 +6,94 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 19:51:41 by jduraes-          #+#    #+#             */
-/*   Updated: 2025/03/12 20:30:31 by jduraes-         ###   ########.fr       */
+/*   Updated: 2025/03/28 19:51:56 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <climits>
 
 Span::Span(unsigned int N) : _N(N)
 {
+	_arr.reserve(N);
 }
 
 Span& Span::operator=(const Span &other)
 {
-	if (_N != other._N)
-		_N = other._N;
+    if (this != &other)
+    {
+        _N = other._N;
+        _arr = other._arr;
+	}
 	return *this;
 }
 
-Span::Span(const Span &other)
+Span::Span(const Span &other): _N(other._N), _arr(other._arr)
 {
-	*this = other;
 }
 
 Span::~Span()
 {
+	/*std::vector<int>::iterator	itt = _arr.end();
+	for(std::vector<int>::iterator	it = _arr.begin(); it != itt; ++it)
+	{
+		delete *it;
+		it = _arr.erase(it);
+	}*/
 }
 
-void	Span::addNumber(unsigned int N)
+void	Span::addNumber(int newn)
 {
+	if (_arr.size() == _N)
+		throw std::runtime_error("Full Span.\n");
+	std::vector<int>::iterator	itt = _arr.end();
+	for(std::vector<int>::iterator	it = _arr.begin(); it != itt; ++it)
+	{
+		if (*it == newn)
+			throw std::runtime_error("Number already in Span.\n");
+	}
+	_arr.push_back(newn);
+}
+
+static size_t abs(int n)
+{
+    return (n < 0) ? -n : n;
+}
+
+unsigned int	Span::shortestSpan()
+{
+	if (_arr.size() <= 1)
+		throw std::runtime_error("No span to be found.\n");
 	
+	size_t	sp = UINT_MAX;
+	std::vector<int>::iterator	ite = _arr.end();
+	std::vector<int>::iterator	it1 = _arr.begin();
+	std::vector<int>::iterator	it2;
+	while (it1 != ite)
+	{
+		it2 = it1;
+		while(++it2 != ite)
+			sp = (::abs((*it1) - (*it2)) < sp) ? ::abs((*it1) - (*it2)) : sp;
+		++it1;
+	}
+	return sp;
+}
+
+unsigned int	Span::longestSpan()
+{
+	if (_arr.size() <= 1)
+		throw std::runtime_error("No span to be found.\n");
+	
+	size_t	sp = 0;
+	std::vector<int>::iterator	ite = _arr.end();
+	std::vector<int>::iterator	it1 = _arr.begin();
+	std::vector<int>::iterator	it2;
+	while (it1 != ite)
+	{
+		it2 = it1;
+		while(++it2 != ite)
+			sp = (::abs((*it1) - (*it2)) > sp) ? ::abs((*it1) - (*it2)) : sp;
+		++it1;
+	}
+	return sp;
 }
--- c++/CPP 08/ex01/main.cpp
+++ c++/CPP 08/ex01/main.cpp
@@ -1,37 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Span.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 19:37:46 by jduraes-          #+#    #+#             */
-/*   Updated: 2025/03/28 19:43:54 by jduraes-         ###   ########.fr       */
+/*   Created: 2025/03/28 19:32:16 by jduraes-          #+#    #+#             */
+/*   Updated: 2025/03/28 19:52:02 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SPAN_HPP
-# define SPAN_HPP
+#include "Span.hpp"
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <exception>
-
-class Span
+int main()
 {
-	private:
-		unsigned int	_N;
-		std::vector<int>	_arr;
-		
-	public:
-		Span(unsigned int N);
-		Span(const Span &other);
-		Span& operator=(const Span &other);
-		~Span();
-		void	addNumber(int newn);
-		unsigned int	shortestSpan();
-		unsigned int	longestSpan();
-};
-
-#endif
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
+}
