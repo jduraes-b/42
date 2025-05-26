c8e33bb36e6e0505746d760922a56785517f0608
jduraes- <jdbarros99@gmail.com>
2025-05-26 20:33:59 +0100

    irc work

--- ft_irc/irc-server/include/client.hpp
+++ ft_irc/irc-server/include/client.hpp
@@ -1,1 +1,53 @@
-// This file intentionally left blank.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 18:27:39 by jduraes-          #+#    #+#             */
+/*   Updated: 2025/05/26 20:27:31 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+#include "server.hpp"
+#include <string>
+#include <vector>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <cerrno>
+
+class	Client
+{
+	private:
+		int	_clientFd;
+		std::string	_nick;
+		std::string	_user;
+		std::string	_currChannel;
+		std::string	_buff;
+		std::string	_pass;
+		bool	_authenticated;
+
+	public:
+		Client(int fd);		
+		~Client();
+		int getFd() const;
+		void setNick(const std::string &nickname);
+		std::string getNick() const;
+		void setUser(const std::string &username);
+		std::string getUser() const;
+		void setPass(const std::string &password);
+		std::string getPass() const;
+		void setCurrChannel(const std::string &channel);
+		std::string getCurrChannel() const;
+		bool isAuthenticated() const;
+		void authenticate();
+		void sendMessage(const std::string &message);
+		std::string receiveMessage();
+		void disconnect();
+};
+
+# endif
--- ft_irc/irc-server/src/client.cpp
+++ ft_irc/irc-server/src/client.cpp
@@ -1,1 +1,117 @@
-// This file is intentionally left blank.
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 18:34:02 by jduraes-          #+#    #+#             */
+/*   Updated: 2025/05/26 20:28:11 by jduraes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client.hpp"
+#include <unistd.h>
+#include <stdexcept>
+#include <cstring>
+#include <iostream>
+
+Client::Client(int fd) : _clientFd(fd), _currChannel(""), _authenticated(false)
+{
+}
+
+Client::~Client()
+{
+    disconnect();
+}
+
+int Client::getFd() const
+{
+    return _clientFd;
+}
+
+void Client::setNick(const std::string &nickname)
+{
+    _nick = nickname;
+}
+
+std::string Client::getNick() const {
+    return _nick;
+}
+
+void Client::setUser(const std::string &username) 
+{
+    _user = username;
+}
+
+std::string Client::getUser() const
+{
+    return _user;
+}
+
+void Client::setPass(const std::string &password) 
+{
+    _pass = password;
+}
+
+std::string Client::getPass() const
+{
+    return _pass;
+}
+
+void Client::setCurrChannel(const std::string &channel)
+{
+    _currChannel = channel;
+}
+
+std::string Client::getCurrChannel() const
+{
+    return _currChannel;
+}
+
+bool Client::isAuthenticated() const
+{
+    return _authenticated;
+}
+
+void Client::authenticate() {
+    _authenticated = true;
+}
+
+void Client::sendMessage(const std::string &message)
+{
+    //the flags stop the client from sending SIGPIPE and make the function non-blocking, respectively
+    //i am not sure how this will behave but it sounds like what the subject demands
+    if (send(_clientFd, message.c_str(), message.size(), MSG_NOSIGNAL | MSG_DONTWAIT) == -1)
+    {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+            throw std::runtime_error("Socket send buffer is full, message could not be sent");
+        else
+            throw std::runtime_error("Failed to send message to client: " + std::string(strerror(errno)));
+    }
+}
+
+std::string Client::receiveMessage()
+{
+    char buffer[512];
+    memset(buffer, 0, sizeof(buffer));
+    int bytesRead = recv(_clientFd, buffer, sizeof(buffer) - 1, MSG_DONTWAIT);
+    if (bytesRead == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            // No data available to read, return an empty string
+            return "";
+        } else {
+            throw std::runtime_error("Failed to receive message from client: " + std::string(strerror(errno)));
+        }
+    } else if (bytesRead == 0) {
+        throw std::runtime_error("Client disconnected");
+    }
+    return std::string(buffer, bytesRead);
+}
+
+void Client::disconnect() {
+    if (_clientFd != -1) {
+        close(_clientFd);
+        _clientFd = -1;
+    }
+}
